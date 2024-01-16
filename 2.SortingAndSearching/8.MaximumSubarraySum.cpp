fa2e972d4c7b0c7feed1e5901a95049b12053945
Jaysanghani08 <sanghanijay36@gmail.com>
2024-01-16 16:01:28 +0530

    Maximum Subarray Sum

---
+++ 2.SortingAndSearching/8.MaximumSubarraySum.cpp
@@ -0,0 +1,59 @@
+// link :
+// soln :
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<vi> vvi;
+typedef pair<ll, ll> pll;
+typedef pair<int, int> pii;
+typedef map<int, int> mii;
+typedef set<int> si;
+#define ld long double
+#define all(a) (a).begin(), (a).end()
+#define PI 3.1415926535897932384626433832795l
+#define fr(i, s, e) for (int i = s; i <= e; ++i)
+#define rfr(i, s, e) for (int i = s; i >= e; --i)
+#define trav(item, x) for (auto &item : x)
+#define pb push_back
+#define mp make_pair
+#define sz(x) (int)(x).size()
+// clang-format off
+#define inv(v) for(auto &i:v){cin>>i;}
+#define endl "\n"
+#define printy cout<<"YES\n";
+#define printn cout<<"NO\n";
+#define print(var) cout<<var<<"\n";
+#define printv(v) for(auto i:v){cout<<i<<" ";}cout<<"\n";
+// clang-format on
+
+void solve()
+{
+    int n;
+    cin >> n;
+
+    vi v(n);
+    inv(v);
+    ll sum = 0, maxSum = INT_MIN;
+    fr(i, 0, n - 1)
+    {
+        sum += v[i];
+        maxSum = max(maxSum, sum);
+        if (sum < 0)
+            sum = 0;
+    }
+
+    print(maxSum);
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    solve();
+
+    return 0;
+}
