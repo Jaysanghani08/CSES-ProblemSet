02ab1f608ac138423907726f7ab1b1b20334f37c
Jaysanghani08 <sanghanijay36@gmail.com>
2024-01-17 07:53:21 +0530

    Traffic Lights

---
+++ 2.SortingAndSearching/15.TrafficLights.cpp
@@ -0,0 +1,69 @@
+// link :
+// soln :
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<vi> vvi;
+typedef pair<ll, ll> pll;
+typedef pair<int, int> pii;
+typedef map<int, int> mii;
+typedef set<int> si;
+#define ld long double
+#define all(a) (a).begin(), (a).end()
+#define PI 3.1415926535897932384626433832795l
+#define fr(i, s, e) for (int i = s; i <= e; ++i)
+#define rfr(i, s, e) for (int i = s; i >= e; --i)
+#define trav(item, x) for (auto &item : x)
+#define pb push_back
+#define mp make_pair
+#define sz(x) (int)(x).size()
+// clang-format off
+#define inv(v) for(auto &i:v){cin>>i;}
+#define endl "\n"
+#define printy cout<<"YES\n";
+#define printn cout<<"NO\n";
+#define print(var) cout<<var<<"\n";
+#define printv(v) for(auto i:v){cout<<i<<" ";}cout<<"\n";
+// clang-format on
+
+void solve()
+{
+    int x, n;
+    cin >> x >> n;
+
+    si pos;
+    multiset<int> len;
+
+    pos.insert(0);
+    pos.insert(x);
+    len.insert(x - 0);
+
+    fr(i, 0, n - 1)
+    {
+        int a;
+        cin >> a;
+
+        auto right = pos.upper_bound(a);
+        auto left = right;
+        left--;
+
+        len.erase(len.find(*right - *left));
+        len.insert(a - *left);
+        len.insert(*right - a);
+        pos.insert(a);
+
+        cout << *(len.rbegin()) << " ";
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    solve();
+    return 0;
+}
