c834b7d813e5194f3dc4de822347e52781faf5c4
Jaysanghani08 <sanghanijay36@gmail.com>
2024-01-17 15:31:55 +0530

    Josephus Problem Il

---
+++ 2.SortingAndSearching/17.JosephusProblemIl.cpp
@@ -0,0 +1,61 @@
+// link :
+// soln :
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+
+typedef tree<int,null_type,less<int>,rb_tree_tag, tree_order_statistics_node_update> indexed_set;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<vi> vvi;
+typedef pair<ll, ll> pll;
+typedef pair<int, int> pii;
+typedef map<int, int> mii;
+typedef set<int> si;
+#define ld long double
+#define all(a) (a).begin(), (a).end()
+#define PI 3.1415926535897932384626433832795l
+#define fr(i, s, e) for (int i = s; i <= e; ++i)
+#define rfr(i, s, e) for (int i = s; i >= e; --i)
+#define trav(item, x) for (auto &item : x)
+#define pb push_back
+#define mp make_pair
+#define sz(x) (int)(x).size()
+// clang-format off
+#define inv(v) for(auto &i:v){cin>>i;}
+#define endl "\n"
+#define printy cout<<"YES\n";
+#define printn cout<<"NO\n";
+#define print(var) cout<<var<<"\n";
+#define printv(v) for(auto i:v){cout<<i<<" ";}cout<<"\n";
+// clang-format on
+
+void solve()
+{
+    int n, k;
+    cin >> n >> k;
+    
+    indexed_set s;
+    fr(i,1,n) s.insert(i);
+    
+    int ind = k%n;
+    
+    while(n--){
+        auto it = s.find_by_order(ind);
+        cout << *it << " ";
+        s.erase(it);
+        
+        if(n) ind = (ind+k)%n;
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    solve();
+    return 0;
+}
