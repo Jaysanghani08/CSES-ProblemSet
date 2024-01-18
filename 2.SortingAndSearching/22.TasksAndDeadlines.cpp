0c719af55701c907aea33bfbd9e356041bdb0976
Jaysanghani08 <sanghanijay36@gmail.com>
2024-01-18 11:58:00 +0530

    Tasks and Deadlines

---
+++ 2.SortingAndSearching/22.TasksAndDeadlines.cpp
@@ -0,0 +1,61 @@
+// link :
+// soln :
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<vi> vvi;
+typedef pair<ll, ll> pll;
+typedef pair<int, int> pii;
+typedef map<int, int> mii;
+typedef set<int> si;
+#define ld long double
+#define all(a) (a).begin(), (a).end()
+#define PI 3.1415926535897932384626433832795l
+#define fr(i, s, e) for (int i = s; i <= e; ++i)
+#define rfr(i, s, e) for (int i = s; i >= e; --i)
+#define trav(item, x) for (auto &item : x)
+#define pb push_back
+#define mp make_pair
+#define sz(x) (int)(x).size()
+// clang-format off
+#define inv(v) for(auto &i:v){cin>>i;}
+#define endl "\n"
+#define printy cout<<"YES\n";
+#define printn cout<<"NO\n";
+#define print(var) cout<<var<<"\n";
+#define printv(v) for(auto i:v){cout<<i<<" ";}cout<<"\n";
+// clang-format on
+
+void solve()
+{
+    int n;
+    cin >> n;
+
+    vector<pii> v(n);
+    fr(i,0,n-1){
+        cin >> v[i].first >> v[i].second;
+    }
+
+    // sort  by length of tasks
+    sort(all(v));
+    ll ans = 0;
+    ll cnt =  0;
+    fr(i,0,n-1){
+        cnt += v[i].first;
+        ans += (v[i].second - cnt);
+    }
+
+    print(ans);
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    solve();
+    return 0;
+}
