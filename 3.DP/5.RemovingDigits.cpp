9beb93cc22966ecdf9af83c9e0bbe561c23809bd
Jaysanghani08 <sanghanijay36@gmail.com>
2024-03-27 18:45:02 +0530

    Removing digits

---
+++ 3.DP/5.RemovingDigits.cpp
@@ -0,0 +1,64 @@
+// link :
+// soln :
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<vi> vvi;
+typedef pair<ll, ll> pll;
+typedef pair<int, int> pii;
+typedef map<int, int> mii;
+typedef set<int> si;
+#define ld long double
+#define all(a) (a).begin(), (a).end()
+#define PI 3.1415926535897932384626433832795l
+#define fr(i, s, e) for (int i = s; i <= e; ++i)
+#define rfr(i, s, e) for (int i = s; i >= e; --i)
+#define trav(item, x) for (auto &item : x)
+#define pb push_back
+#define mp make_pair
+#define sz(x) (int)(x).size()
+// clang-format off
+#define inv(v) for(auto &i:v){cin>>i;}
+#define endl "\n"
+#define printy cout<<"YES\n";
+#define printn cout<<"NO\n";
+#define print(var) cout<<var<<"\n";
+#define printv(v) for(auto i:v){cout<<i<<" ";}cout<<"\n";
+// clang-format on
+
+void solve()
+{
+    ll n;
+    cin >> n;
+
+    // print("ghddf");
+    vector<ll> dp(n + 1, INT_MAX);
+
+    dp[0] = 0;
+    fr(curr, 1, n)
+    {
+        ll mini = INT_MAX;
+        for (char digit : to_string(curr))
+            mini = min(mini, dp[curr - (digit - '0')] + 1);
+        
+        dp[curr] = mini;
+    }
+
+    print(dp[n]);
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    // int t;
+    // cin >> t;
+    // while(t--){
+    solve();
+    // }
+    return 0;
+}
